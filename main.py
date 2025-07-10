47cecdc51895fabff7c4e8dc6531fc0db3978324
Dharshana Somasunderam <sdharsha@umich.edu>
2025-07-10 19:10:29 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from loaders import load_csv
+from cleaners import clean_data
+
+
+def run_pipeline():
+    df = load_csv("./data/data.csv")
+    df_clean = clean_data(df)
+    print(df_clean.head())
+
 if __name__ == "__main__":
-    print("Hello World")
+    run_pipeline()
