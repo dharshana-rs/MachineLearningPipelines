31c34aba5c765bec5b7ccfc70098139f041ea979
Dharshana Somasunderam <sdharsha@umich.edu>
2025-07-10 19:20:14 -0400

    Updated main with args and added ascii loader

--- loaders.py
+++ loaders.py
@@ -9,3 +9,16 @@ def load_csv(path):
         pd.DataFrame: Loaded data.
     """
     return pd.read_csv(path)
+
+
+def load_ascii(path, colspecs, colnames):
+    """
+    Load data from a fixed width ASCII file.
+    Args:
+        path (str): Path to the CSV file.
+        colspecs (array-like): Column specifications.
+        colnames (array-like): Column names.
+    Returns:
+        pd.DataFrame: Loaded data.
+    """
+    return pd.read_fwf(path, colspecs=colspecs, names=colnames)
--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-from loaders import load_csv
+from loaders import load_csv, load_ascii
 from cleaners import clean_data
+import argparse
 
 
-def run_pipeline():
-    df = load_csv("./data/data.csv")
+def run_pipeline(csv_path: str, ascii_path: str):
+    print("\nCSV Files")
+    df = load_csv(csv_path)
     df_clean = clean_data(df)
     print(df_clean.head())
 
+    print("\nASCII Files")
+    colspecs = [(0, 2), (2, 12), (12, 24)]
+    colnames = ["id", "job_title", "location"]
+    df_fwf = load_ascii(ascii_path, colspecs, colnames)
+    df_fwf_clean = clean_data(df_fwf)
+    print(df_fwf_clean.head())
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run data pipeline for CSV and ASCII files.")
+    parser.add_argument(
+        "--csv",
+        type=str,
+        default="./data/data.csv",
+        help="Path to the CSV file",
+    )
+    parser.add_argument(
+        "--ascii",
+        type=str,
+        default="./data/data_ascii.txt",
+        help="Path to the ASCII (fixed-width) file",
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    run_pipeline()
+    args = parse_args()
+    run_pipeline(args.csv, args.ascii)
